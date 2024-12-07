451790e8f4f1e3407910a22d14bb0ae3a2dbb29c
raycastillo3 <60750068+raycastillo3@users.noreply.github.com>
2024-12-07 00:18:48 -0500

    added all employees

--- src/EmployeeList.tsx
+++ src/EmployeeList.tsx
@@ -2,8 +2,12 @@ import EmployeeListItem from "./EmployeeListItem"
 export default function EmployeeList () {
     return (
         <div className="employeeList"> 
-        <EmployeeListItem img={"https://www.w3schools.com/howto/img_avatar.png"} alt={"James King"} name={"James King"} title={"President and CEO"}/>
-        <EmployeeListItem img={"https://www.w3schools.com/howto/img_avatar2.png"} alt={"Julie Taylor"} name={"Julie Taylor"} title={"VP of Marketing"}/>
+        <EmployeeListItem img={"https://shorturl.at/sgAV9"} alt={"James King"} name={"James King"} title={"President and CEO"}/>
+        <EmployeeListItem img={"https://shorturl.at/V16wg"} alt={"Julie Taylor"} name={"Julie Taylor"} title={"VP of Marketing"}/>
+        <EmployeeListItem img={"https://shorturl.at/LAvxB"} alt={"Eugene Lee"} name={"Eugene Lee"} title={"CFO"}/>
+        <EmployeeListItem img={"https://shorturl.at/ScKbq"} alt={"John Williams"} name={"John Williams"} title={"VP of Engineering"}/>
+        <EmployeeListItem img={"https://shorturl.at/VpBvS"} alt={"Ray Moore"} name={"Ray Moore"} title={"VP of Sales"}/>
+        <EmployeeListItem img={"https://tinyurl.com/5n76a482"} alt={"Paul Jones"} name={"Paul Jones"} title={"QA Manager"}/>
         </div>
     )
 }
