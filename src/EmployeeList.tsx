fdbb0377c46fd76a9d1bb1ea299ee42b8eb41612
raycastillo3 <60750068+raycastillo3@users.noreply.github.com>
2024-12-06 23:57:00 -0500

    added props

--- src/EmployeeList.tsx
+++ src/EmployeeList.tsx
@@ -2,8 +2,13 @@ import EmployeeListItem from "./EmployeeListItem"
 export default function EmployeeList () {
     return (
         <div className="employeeList"> 
-        <EmployeeListItem/>
+        <EmployeeListItem img={"https://www.w3schools.com/howto/img_avatar.png"} alt={"James King"} name={"James King"} title={"President and CEO"}/>
+        <EmployeeListItem img={"https://www.w3schools.com/howto/img_avatar2.png"} alt={"Julie Taylor"} name={"Julie Taylor"} title={"VP of Marketing"}/>
         </div>
     )
 }
+
+
+    
+     
 
--- src/EmployeeListItem.tsx
+++ src/EmployeeListItem.tsx
@@ -1,58 +1,13 @@
-export default function EmployeeListItem() {
+export default function EmployeeListItem(props: any) {
   return (
     <div className="employeeListItems">
       <div className="profilecard">
         <div className="avatar">
-          <img src="https://www.w3schools.com/howto/img_avatar.png" alt="James King" />
+          <img src={props.img} alt={props.alt} />
         </div>
         <div className="information">
-          <h3>James King</h3>
-          <p>President and CEO</p>
-        </div>
-      </div>
-      <div className="profilecard">
-        <div className="avatar">
-          <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="James King" />
-        </div>
-        <div className="information">
-          <h3>Julie Taylor</h3>
-          <p>VP of Marketing</p>
-        </div>
-      </div>
-      <div className="profilecard">
-        <div className="avatar">
-          <img src="https://www.w3schools.com/howto/img_avatar.png" alt="James King" />
-        </div>
-        <div className="information">
-          <h3>Eugene Lee</h3>
-          <p>CFO</p>
-        </div>
-      </div>
-      <div className="profilecard">
-        <div className="avatar">
-          <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="James King" />
-        </div>
-        <div className="information">
-          <h3>John Williams</h3>
-          <p>VP of Engineering</p>
-        </div>
-      </div>
-      <div className="profilecard">
-        <div className="avatar">
-          <img src="https://www.w3schools.com/howto/img_avatar.png" alt="James King" />
-        </div>
-        <div className="information">
-          <h3>Ray Moore</h3>
-          <p>VP of Sales</p>
-        </div>
-      </div>
-      <div className="profilecard">
-        <div className="avatar">
-          <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="James King" />
-        </div>
-        <div className="information">
-          <h3>Ray Moore</h3>
-          <p>VP of Sales</p>
+          <h3>{props.name}</h3>
+          <p>{props.title}</p>
         </div>
       </div>
     </div>
